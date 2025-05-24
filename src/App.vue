0ace14de38192f885ab14654e42cd439660e1d5c
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2025-05-24 15:24:45 +0100

    use tradeanalaysis component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
+<template>
+  <div class="flex w-screen overflow-y-hidden max-h-screen">
+    <TNavbar />
+    <main class="grow flex-1 p-4 px-8 lg:px-16 overflow-y-auto">
+      <TradeAnalysis v-if="chartData" :chart-data="chartData" />
+    </main>
+  </div>
+  <FullPageLoader />
+</template>
+
 <script setup lang="ts">
-import { inject, onMounted } from "vue";
-  
+  import { inject, onMounted, ref } from "vue";
+
+  import FullPageLoader from "./components/FullPageLoader.vue";
   import TNavbar from "./components/TNavbar.vue";
-  import HighchartDemo from "./components/HighchartDemo.vue";
+  import TradeAnalysis from "./components/charts/TradeAnalysis.vue";
+
   import { ChartDataApiService } from "./services";
+  import { ChartData } from "./types/chart.types";
 
   const chartDataApiService = inject(
     "chartDataApiService"
   ) as ChartDataApiService;
 
+  const chartData = ref<ChartData | null>(null);
+
+  const getChartData = async () => {
+    chartData.value = (await chartDataApiService.getChartData()).data;
+  };
+
   onMounted(async () => {
-    const chartData = await chartDataApiService.getChartData();
-    console.log(chartData);
+    await getChartData();
   });
 </script>
-
-<template>
-  <div class="flex w-screen overflow-y-hidden max-h-screen">
-    <TNavbar />
-    <main class="grow flex-1 p-4 px-8 lg:px-16 overflow-y-auto">
-      <HighchartDemo />
-    </main>
-  </div>
-</template>
---
+++ src/components/charts/TradeAnalysis.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <pre>{{ chartData }}</pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ChartData } from "../../types/chart.types";
+
+  const props = defineProps<{
+    chartData: ChartData;
+  }>();
+</script>
