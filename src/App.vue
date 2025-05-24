689917e1981369df55026f2530c22ed8e2ab63a1
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2025-05-24 11:52:40 +0100

    demo chart

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-  import HelloWorld from "./components/HelloWorld.vue";
-  import TNavbar from "./components/TNavbar.vue";
+import TNavbar from "./components/TNavbar.vue";
+import HighchartDemo from "./components/HighchartDemo.vue";
+  
 </script>
 
 <template>
   <div class="flex w-screen overflow-y-hidden max-h-screen">
     <TNavbar />
     <main class="grow flex-1 p-4 px-8 lg:px-16 overflow-y-auto">
-      <HelloWorld id="title" title="Welcome to the app!" />
-      <HelloWorld id="second-title" title="Another Section" />
-      <HelloWorld id="third-title" title="Another Section With Long Title" />
-      <HelloWorld id="fourth-title" title="This is another title" />
-      <HelloWorld id="final-title" title="This is the final title" />
+      <HighchartDemo />
     </main>
   </div>
 </template>
---
+++ src/components/HighchartDemo.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <highcharts :options="chartOptions" />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  const chartOptions = ref({
+    chart: {
+      type: "line",
+      backgroundColor: "#262627",
+      borderRadius: 10,
+    },
+    title: {
+      text: "Demo Highcharts Line Chart",
+      style: {
+        color: "white",
+      },
+    },
+    xAxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+      labels: {
+        style: {
+          color: "white",
+        },
+      },
+    },
+    yAxis: {
+      title: {
+        text: "Value",
+        style: {
+          color: "white",
+        },
+      },
+      labels: {
+        style: {
+          color: "#666",
+        },
+      },
+    },
+    series: [
+      {
+        type: "spline",
+        name: "Demo Series",
+        data: Array.from({ length: 12 }, (_, i) =>
+          Math.floor(Math.random() * 25 + i ** 1.3)
+        ),
+        color: "#65C49D",
+        marker: {
+          enabled: false,
+        },
+        style: {
+          color: "white",
+        },
+      },
+    ],
+  });
+</script>
