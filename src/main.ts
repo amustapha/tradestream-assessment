83c93dc0fbac694888b15cf5972706a96607bbb2
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2025-05-24 15:10:29 +0100

    add state management

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import "./style.css";
 import App from "./App.vue";
 import HighchartsVue from "highcharts-vue";
 import { ChartDataApiService } from "./services";
 
 const app = createApp(App);
+const pinia = createPinia();
 
+app.use(pinia);
 app.use(HighchartsVue);
 app.provide(
   "chartDataApiService",
