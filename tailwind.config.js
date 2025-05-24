420abd4b8d9db426c323f41b9591f350d2c10be3
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2025-05-24 07:17:25 +0100

    setup averta font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Averta", "system-ui", "sans-serif"],
+      },
+      fontWeight: {
+        thin: "100",
+        extralight: "200",
+        light: "300",
+        normal: "400",
+        semibold: "600",
+        bold: "700",
+        extrabold: "800",
+        black: "900",
+      },
+    },
   },
   plugins: [],
 };
